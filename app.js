826eee41ca9a1c1b5572f12d62aaca61ea921ad2
Sam Johnston <johnstonsam712@gmail.com>
2020-06-18 17:33:12 +0100

    *Making the posts router a middleware

--- app.js
+++ app.js
@@ -2,20 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 require('dotenv/config');
-//MIDDLEWARE (used to run functions when a specific route is hit)
-app.use('/posts', () =>{
-    console.log('middleware function run when /posts is hit')
-});
+
+//import the posts router
+const postRouter = require('./routes/posts')
+app.use('/posts', postRouter);
+
 
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Home');
 });
 
-
-app.get('/posts', (req, res) => {
-    res.send('Posts');
-});
 //open the server
 app.listen(3000);
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Posts');
+});
+
+module.exports = router;
